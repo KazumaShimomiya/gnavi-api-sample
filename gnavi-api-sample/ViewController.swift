28e62a485b18bab5dd79029d4df104e5ab9ddb0c
KazumaShimomiya <shimomiya_001@yahoo.co.jp>
2017-03-28 10:08:24 +0900

    first project comment

---
+++ gnavi-api-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gnavi-api-sample
+//
+//  Created by Shimomiya Kazuma on 2017/03/28.
+//  Copyright © 2017年 kshimomiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
